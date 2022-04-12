65acbecb470a09adc9fcf0fdff26488eca148925
liztoledo <lizeditht@gmail.com>
2022-04-11 20:04:30 -0700

    Create python_fundamentals.py
    Added Python file for Python Variables and Data Types Assignment

---
+++ python_fundamentals.py
@@ -0,0 +1,19 @@
+print("Hello World") #Print statement saying 'Hello World'
+
+# Step I Instructions
+name = "Henry"
+age = 25
+years_at_birth = 1.5
+plays_basketball = True
+jersey_number = "13"
+
+# Step II Instructions
+car_name = "Honda"
+x = 50
+first_name = "Liz"
+
+print(type(car_name))
+print(type(x))
+print(type(first_name))
+
+# Step III Instructions
