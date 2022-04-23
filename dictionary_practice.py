48eb7c989fe9a626c14b05db1d7fd131fac71935
liztoledo <lizeditht@gmail.com>
2022-04-23 14:31:25 -0700

    Update dictionary_practice.py

---
+++ dictionary_practice.py
@@ -0,0 +1,18 @@
+#STEP II
+grocery_prices = {"Chicken": 1.59, "Beef":1.99, "Cheese":1.0, "Milk": 2.5}
+
+#STEP III
+birth_years = {"Tais": 2009, "Liz": 2000, "Uli": 1990}
+
+#STEP IV
+chicken = grocery_prices["Chicken"]
+beef = grocery_prices["Beef"]
+cheese = grocery_prices["Cheese"]
+milk = grocery_prices["Milk"]
+print(chicken, beef, cheese, milk)
+
+#STEP V
+tais = birth_years["Tais"]
+liz = birth_years["Liz"]
+uli = birth_years["Uli"]
+print(tais, liz, uli)
