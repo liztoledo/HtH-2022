3434cbc81cab5fbbf7ad5aab1366aeec81641afa
liztoledo <lizeditht@gmail.com>
2022-06-11 13:03:41 -0700

    Update checklist.py

--- checklist.py
+++ checklist.py
@@ -12,14 +12,17 @@ def create(item):
 
 def read(index):
     # Read code here
-    return checklist[index]
+    index = int(index)
+    print(checklist[index])
 
 def update(index, item):
     # Update code here
+    index = int(index)
     checklist[index] = item
 
 def destroy(index):
     # Destroy code here
+    index = int(index)
     del checklist[index]
 
 def mark_completed(index):
@@ -28,35 +31,72 @@ def mark_completed(index):
 
 def list_all_items():
     # List all items code here
-    print(checklist)
+    completed = 0
+    incompleted = []
+
+    return checklist
 
 def user_input(prompt):
     # Get user input here
     return input(prompt)
 
-
 def select(function_code):
     # User Selection Code here
+
     # Create item example
-    if function_code == "C":
+    #User selected P to print list
+    if function_code == "P":
         input_item = user_input("Input item:")
         create(input_item)
+        running = True
+        return running
+    
+    #User selected A to add to list
+    if function_code == "A":
+        input_item = user_input("Input item:")
+        create(input_item)
+        running = True
+        return running
+        
+    # User selected D to delete item from list
+    elif function_code == "D":
+        input_index = user_input("Select index:")
+        running = True
+        return running
+
     # User selected Read fcn
     elif function_code == "R":
         input_index = user_input("Select index:")
         read(input_index)
+        running = True
+        return running
+
     # User selected update fcn
     elif function_code == "U":
         input_index = user_input("Select index:")
         input_item = user_input("Input item:")
         update(input_index, input_item)
-    # User selected destroy fcn
-    elif function_code == "D":
-        input_index = user_input("Select index:")
+        running = True
+        return running
+
+    #Unvalid Command
+    else:
+        print("Please enter a valid checklist command.")
+        running = True
+        return running
 
 running = True
 
 while running:
     selection = user_input(
-        "Press C to add to list, R to Read from list, P to display list, and Q to quit")
+        "Press one of the following \n P to display list \n A to add to list \n D to delete from list \n U to replace an item from list \n R to Read from list \n and Q to quit")
     running = select(selection)
+
+
+#What I need to complete
+#P command to display list
+#Q command
+#Include mark_completed and list_all_items in select function
+#Add U, D to selestion inputs
+#Work on the list_all_items
+
